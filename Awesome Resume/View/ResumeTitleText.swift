4af9041bba25b36f10e452af1cf33bc747ad0cb7
BOYA CHEN <boyachen@Master.local>
2017-09-05 00:28:39 +1000

    second try

---
+++ Awesome Resume/View/ResumeTitleText.swift
@@ -0,0 +1,14 @@
+//
+//  ResumeTitleText.swift
+//  Awesome Resume
+//
+//  Created by BOYA CHEN on 4/9/17.
+//  Copyright © 2017 Awesome Team. All rights reserved.
+//
+
+import UIKit
+
+class ResumeTitleText: UIView {
+    
+    
+}
